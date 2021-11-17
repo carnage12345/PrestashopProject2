3135afc2df5f02ebfc1fe23d7c3dcd1deebb1cc8
Kamil118 <Kamil1mld@gmail.com>
2021-11-17 21:36:46 +0000

    first commit

---
+++ var/cache/prod/smarty/cache/ps_banner/1/1/1/14/classic/b4/34/a4/b434a49c202fd63ec8a1cd736cae30f7fa7c3553.psbannerpsbanner.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-17 22:17:28
+  from 'module:psbannerpsbanner.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_61957168aec571_10011942',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '64034093fbda864710a2c0f526b37f921e53ee39' => 
+    array (
+      0 => 'module:psbannerpsbanner.tpl',
+      1 => 1633363913,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_61957168aec571_10011942 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="https://localhost:8080/" title="">
+      <img src="https://localhost:8080/modules/ps_banner/img/sale70.png" alt="" title="" class="img-fluid" loading="lazy" width="1110" height="213">
+  </a>
+<?php }
+}
